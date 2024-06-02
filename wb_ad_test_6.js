8ec9f601e2f30a14165866efbae8b19f47a37e03
Zhiyuan Ma <631253076@qq.com>
2024-06-01 23:00:15 -0400

    Update wb_ad_test_6.js

--- wb_ad_test_6.js
+++ wb_ad_test_6.js
@@ -5,9 +5,9 @@ let obj = JSON.parse(body);
 if (obj.items && obj.items.length > 0) {
     let i = obj.items.length;
     while (i--) {
-        if(obj.items[i].data && obj.items[i].data.mblogtype && obj.items[i].data.mblogtype == 1) {
+        if (obj.items[i].data && obj.items[i].data.mblogtype && obj.items[i].data.mblogtype == 1) {
             obj.items.splice(i, 1);
-        }else if (isAd(obj.items[i])) {
+        } else if (isAd(obj.items[i])) {
             obj.items.splice(i, 1);
         }
     }
@@ -30,4 +30,4 @@ function isAd(item) {
 }
 
 body = JSON.stringify(obj);
-$done({ body });
+$done({body});
