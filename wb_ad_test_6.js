a3467a5da754e61c3cc0653e8e5f8d69d3fdc0e1
Zhiyuan Ma <631253076@qq.com>
2024-09-26 03:40:51 -0400

    Update wb_ad_test_6.js

--- wb_ad_test_6.js
+++ wb_ad_test_6.js
@@ -7,8 +7,6 @@ if (obj.items && obj.items.length > 0) {
     while (i--) {
         if (obj.items[i].data && obj.items[i].data.mblogtype && obj.items[i].data.mblogtype === 1) {
             obj.items.splice(i, 1);
-        } else if (isAd(obj.items[i])) {
-            obj.items.splice(i, 1);
         }
     }
 }
@@ -23,12 +21,6 @@ function isAd(item) {
             }
             if (cur.data && cur.data.title_extra_text && cur.data.title_extra_text === "\u5e7f\u544a") {
                 return true;
-            }
-            if (cur.data.mblogtypename === '广告' || cur.data.mblogtypename === '热推') {
-                return true
-            }
-            if (cur.data.promotion && cur.data.promotion.type === 'ad') {
-                return true
             }
         }
     }
