fe2183e36871653cb7046052a59f9133a933178a
Zhiyuan Ma <631253076@qq.com>
2026-03-01 23:33:31 -0800

    Simplify ad removal logic: only use mblogtype === 1 for both formats

--- wb_ad_test_10.js
+++ wb_ad_test_10.js
@@ -5,7 +5,14 @@ let obj = JSON.parse(body);
 if (obj.items && obj.items.length > 0) {
   let i = obj.items.length;
   while(i--) {
+    // Old logic for legacy format
     if(obj.items[i].status && obj.items[i].status.mblogtype === 1) {
+      obj.items.splice(i, 1);
+      continue;
+    }
+    // New logic for current format
+    let data = obj.items[i].data;
+    if (data && data.mblogtype === 1) {
       obj.items.splice(i, 1);
     }
   } 
